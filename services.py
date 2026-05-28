3c52230571465368022d4ef025077839018b12b0
apc24 <ko_masuda@mail.toyota.co.jp>
2026-05-28 13:30:29 +0900

    debug出力部分の割り当て修正

--- services.py
+++ services.py
@@ -94,7 +94,7 @@ services: dict[str, tuple] = {
   "livestreamWideRoadEncodeData": (False, 20.),
   "livestreamRoadEncodeData": (False, 20.),
   "livestreamDriverEncodeData": (False, 20.),
-  "customReservedRawData0": (True, 0.),
+  "E2EOutput": (True, 0.),
   "customReservedRawData1": (True, 0.),
   "customReservedRawData2": (True, 0.),
 }
